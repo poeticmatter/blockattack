2addb1fdd1fec1e9d12a7c88cf7c12b02516d98b
Itai Raccah <d00m.poet@gmail.com>
2018-01-06 20:25:28 +0200

    two blocks moving

---
+++ Assets/Code/Block.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	public Vector2 destination;
+	private Vector2 velocity = Vector2.zero;
+	public float maxVelocity;
+	public float smoothTime;
+	public int minY;
+	public int maxY;
+	public int minX;
+	public int maxX;
+
+	void Start () {
+		BlocksManager.instance.grid[(int)destination.x, (int)destination.y] = this;
+	}
+	
+	
+	void Update () {
+		transform.position = Vector2.SmoothDamp(transform.position, destination, ref velocity, smoothTime, maxVelocity, Time.deltaTime);
+	}
+
+	public void Move(Vector2 destination)
+	{
+		this.destination = destination;
+	}
+}
---
+++ Assets/Code/BlocksManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksManager : MonoBehaviour {
+
+	public static BlocksManager instance;
+
+	void Awake ()
+	{
+		if (instance != null)
+		{
+			Debug.LogError("More than one BlockManager");
+		}
+		instance = this;
+	}
+
+	public Block[,] grid = new Block [8,8];
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			for (int x = 0; x < grid.GetLength(0); x++)
+			{
+				MoveUp(x);
+			}
+		} else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			for (int x = 0; x < grid.GetLength(0); x++)
+			{
+				MoveDown(x);
+			}
+		} else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			for (int y = 0; y < grid.GetLength(1); y++)
+			{
+				MoveRight(y);
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			for (int y = 0; y < grid.GetLength(1); y++)
+			{
+				MoveLeft(y);
+			}
+		}
+
+	}
+
+	private void MoveUp(int x)
+	{
+		for (int y = grid.GetLength(1)-1; y >= 0; y--)
+		{
+			MoveYRecursive(1, 0, grid.GetLength(1)-1, x, y);
+		}
+	}
+
+	private void MoveDown(int x)
+	{
+		for (int y = 0; y < grid.GetLength(1); y++)
+		{
+			MoveYRecursive(-1, 0, grid.GetLength(1)-1, x, y);
+		}
+	}
+
+	private void MoveYRecursive(int yDirection, int minY, int maxY, int x, int y)
+	{
+		if (y < minY || y > maxY)
+		{
+			return;
+		}
+		if(grid[x, y] == null)
+		{
+			return;
+		}
+		maxY = grid[x, y].maxY;
+		minY = grid[x, y].minY;
+		if (y + yDirection < minY || y + yDirection > maxY)
+		{
+			return;
+		}
+		if (grid[x, y + yDirection] == null)
+		{
+			grid[x, y].Move(new Vector2(x, y + yDirection));
+			grid[x, y + yDirection] = grid[x, y];
+			grid[x, y] = null;
+			MoveYRecursive(yDirection, minY, maxY, x, y + yDirection);
+		}
+	}
+
+	private void MoveRight(int y)
+	{
+		for (int x = grid.GetLength(0) - 1; x >= 0; x--)
+		{
+			MoveXRecursive(1, 0, grid.GetLength(0) - 1, x, y);
+		}
+	}
+
+	private void MoveLeft(int y)
+	{
+		for (int x = 0; x < grid.GetLength(0); x++)
+		{
+			MoveXRecursive(-1, 0, grid.GetLength(0) - 1, x, y);
+		}
+	}
+
+	private void MoveXRecursive(int xDirection, int minX, int maxX, int x, int y)
+	{
+		if (x < minX || x > maxX)
+		{
+			return;
+		}
+		if (grid[x, y] == null)
+		{
+			return;
+		}
+		maxX = grid[x, y].maxX;
+		minX = grid[x, y].minX;
+		if (x + xDirection < minX || x + xDirection > maxX)
+		{
+			return;
+		}
+		if (grid[x + xDirection, y] == null)
+		{
+			grid[x, y].Move(new Vector2(x + xDirection, y));
+			grid[x + xDirection, y] = grid[x, y];
+			grid[x, y] = null;
+			MoveXRecursive(xDirection, minX, maxX, x + xDirection, y);
+		}
+	}
+
+
+}
